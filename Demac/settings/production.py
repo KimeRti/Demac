8c828eb645d170d2c035b499fbbbea4caba40acb
Ahmet <112516305+KimeRti@users.noreply.github.com>
2024-03-01 18:36:06 +0300

    fix

--- Demac/settings/production.py
+++ Demac/settings/production.py
@@ -5,6 +5,11 @@ DEBUG = True
 ALLOWED_HOSTS = ['45.147.46.199', 'demacimportexport.com', 'www.demacimportexport.com']
 STATIC_ROOT = 'Demac/static/'
 
+CSRF_TRUSTED_ORIGINS = ['http://www.demacimportexport.com', 'http://demacimportexport.com', 'https://www.demacimportexport.com', 'https://demacimportexport.com']
+CORS_ALLOWED_ORIGINS = ['http://www.demacimportexport.com', 'http://demacimportexport.com', 'https://www.demacimportexport.com', 'https://demacimportexport.com']
+CORS_ALLOWED_ORIGIN_REGEXES = ['http://www.demacimportexport.com', 'http://demacimportexport.com', 'https://www.demacimportexport.com', 'https://demacimportexport.com']
+CORS_ALLOW_ALL_ORIGINS = ['http://www.demacimportexport.com', 'http://demacimportexport.com', 'https://www.demacimportexport.com', 'https://demacimportexport.com']
+
 
 DATABASES = {
     'default': {
